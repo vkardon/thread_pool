dd3b1ef078e350c4cf8b4f1c35b2cffd9d8c3164
vkardons <vkardons@advent.com>
2023-10-13 16:36:59 -0500

    Add example for 'pointer to member function'

--- example.cpp
+++ example.cpp
@@ -2,11 +2,11 @@
 // example.cpp
 //
 #include <iostream>
-#include "ThreadPool.hpp"
+#include "threadPool.hpp"
 
 static std::mutex sLogMutex;
 
-auto fptr1 = [](int count)
+auto func1 = [](int count)
 {
     // Do something here...
     usleep((random() % 5) * 1000); // Add a random 0-4 ms delay
@@ -14,7 +14,7 @@ auto fptr1 = [](int count)
     std::cout << "[tid=" << ThreadPool::GetThreadId() << "] count=" << count << std::endl;
 };
 
-auto fptr2 = [](int count, const std::string& str)
+void func2(int count, const std::string& str)
 {
     // Do something here...
     usleep((random() % 5) * 1000); // Add a random 0-4 ms delay
@@ -23,26 +23,54 @@ auto fptr2 = [](int count, const std::string& str)
             << ", str='" << str << "'"<< std::endl;
 };
 
+class MyClass
+{
+public:
+    void func3(int count)
+    {
+        // Do something here...
+        usleep((random() % 5) * 1000); // Add a random 0-4 ms delay
+        std::unique_lock<std::mutex> lock(sLogMutex);
+        std::cout << "[tid=" << ThreadPool::GetThreadId() << "] count=" << count
+                << ", name='" << name << "'"<< std::endl;
+    }
+    std::string name{"MyClass"};
+};
+
 int main()
 {
     ThreadPool tpool;
     tpool.Create(4); // 4 threads
 
-    // Test thread pool using fptr1
+    // Test thread pool with lambda exression (func1)
+    std::cout << ">>> Begin Of ThreadPool 'lambda exression' test" << std::endl;
     for(int i = 0; i < 20; i++)
     {
-        tpool.Post(fptr1, i);
+        tpool.Post(func1, i);
     }
     tpool.Wait();
-    std::cout << ">>> " << __func__ << ": End Of TestThreadPool part 1" << std::endl;
+    std::cout << ">>> End Of ThreadPool 'lambda exression' test" << std::endl;
 
-    // Test thread pool using fptr2
+    // Test thread pool with function pointer (func2)
+    std::cout << ">>> Begin Of ThreadPool 'function pointer' test" << std::endl;
     for(int i = 0; i < 20; i++)
     {
-        tpool.Post(fptr2, i, "hello" + std::to_string(i));
+        tpool.Post(func2, i, "hello" + std::to_string(i));
     }
     tpool.Wait();
-    std::cout << ">>> " << __func__ << ": End Of TestThreadPool part 2" << std::endl;
+    std::cout << ">>> End Of ThreadPool 'function pointer' test" << std::endl;
+
+    // Test thread pool with pointer to member function (MyClass::func3)
+    std::cout << ">>> Begin Of ThreadPool 'pointer to member function' test" << std::endl;
+    MyClass obj;
+    for(int i = 0; i < 20; i++)
+    {
+        tpool.Post(&MyClass::func3, &obj, i);
+    }
+    tpool.Wait();
+    std::cout << ">>> End Of ThreadPool 'pointer to member function' test" << std::endl;
+
+
     return 0;
 }
 
